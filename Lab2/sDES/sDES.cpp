3d6e28dac48ec2687e48c0e6b768ca7b50d361e8
sasha.sokolovsky01 <sasha.sokolovsky01@gmail.com>
2022-02-28 01:20:36 +0300

    Web des

--- Lab2/sDES/sDES.cpp
+++ Lab2/sDES/sDES.cpp
@@ -2,6 +2,9 @@
 //
 
 #include <iostream>
+#include <Windows.h>
+#include <fstream>
+
 const int P10[10] = {3,5,2,7,4,10,1,9,8,6};
 const int P8[8] = {6,3,7,4,8,5,10,9};
 const int shift1[5] = { 1,2,3,4,0 };
@@ -58,33 +61,100 @@ void keyGen()
 	}
 	
 }
+char* readPlain(int position=0, int count=6830) {
+	std::ifstream file;
+	file.open("C:\\Users\\DELL\\source\\repos\\KIOKI\\Lab2\\sDES\\plainText.txt", std::ios_base::binary);
 
+	if (!file) std::cout << "Файл не открыт\n\n";
+	else std::cout << "Все ОК! Файл открыт!\n\n";
+
+	
+	if (!file.is_open()) return 0;
+	
+	if (file.eof()) return 0;
+	char* buffer = new char[count];
+	file.read(buffer, count);
+	file.close();
+	return buffer;
+}
+void CharToByte(char* chars, byte* bytes, unsigned int count) {
+	for (unsigned int i = 0; i < count; i++)
+		bytes[i] = (byte)chars[i];
+}
+
+void ByteToChar(byte* bytes, char* chars, unsigned int count) {
+	for (unsigned int i = 0; i < count; i++)
+		chars[i] = (char)bytes[i];
+}
+void crypt()
+{
+	std::ifstream file;
+	file.open("C:\\Users\\DELL\\source\\repos\\KIOKI\\Lab2\\sDES\\plainText.txt", std::ios_base::binary);
+
+	if (!file) std::cout << "Файл не открыт\n\n";
+	else std::cout << "Все ОК! Файл открыт!\n\n";
+
+
+	
+	char* buffer = new char[6830];
+	file.read(buffer, 6830);
+
+
+	char** cryptArr = new char *[854];
+	for(int i=0;i<854;i++)
+	{
+		cryptArr[i] = new char[8];
+	}
+
+
+	
+	file.close();
+
+
+
+	
+}
 int main()
 {
 	setlocale(LC_ALL, "RU");
-    std::cout << "Введите ключ!\n";
-	int inputKey;
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+ //   std::cout << "Введите ключ!\n";
+	//int inputKey;
+	//
+	//std::cin >> inputKey;
 
-	std::cin >> inputKey;
+	//for(int i=0,j=9;i<10;i++,j--)
+	//{
+	//	inKey[j] = (int)inputKey % 10;
+	//	inputKey /= 10;
+	//}
+	//keyGen();
+	//
+	//std::cout << "К1: \n";
+	//for (int i = 0; i < 8; i++)//перестановка по Р10
+	//{
+	//	std::cout << key1[i];
+	//}
 
-	for(int i=0,j=9;i<10;i++,j--)
+	//std::cout << "\nК2: \n";
+	//for (int i = 0; i < 8; i++)//перестановка по Р10
+	//{
+	//	std::cout << key2[i];
+	//}
+	std::ifstream file;
+	file.open("C:\\Users\\DELL\\source\\repos\\KIOKI\\Lab2\\sDES\\plainText.txt");
+
+	char* plainText = readPlain();
+
+
+	for(int i=0;i<6830;i++)
 	{
-		inKey[j] = (int)inputKey % 10;
-		inputKey /= 10;
+		std::cout << plainText[i];
 	}
-	keyGen();
 	
-	std::cout << "К1: \n";
-	for (int i = 0; i < 8; i++)//перестановка по Р10
-	{
-		std::cout << key1[i];
-	}
-
-	std::cout << "\nК2: \n";
-	for (int i = 0; i < 8; i++)//перестановка по Р10
-	{
-		std::cout << key2[i];
-	}
+	
+	file.close();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
